a091aed641920e7358e1c37a1159c676923587e3
carlo bactol <carloba@main.mpi>
2023-08-04 09:59:27 +0800

    Initialize

---
+++ StackOverflowProject.DomainModel/Question.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StackOverflowProject.DomainModel
+{
+    public class Question
+    {
+        [Key] // set primary key
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // set identity id column 
+
+        public int QuestionID { get; set; }
+        public string QuestionName { get; set; }
+        public DateTime QuestionDateAndTime { get; set; }
+        public int UserID { get; set; }
+        public int CategoryID { get; set; }
+        public int VotesCount { get; set; }
+        public int AnswersCount { get; set; }
+        public int ViewsCount { get; set; }
+
+
+        // Bind User model 
+        [ForeignKey("UserID")]
+        public virtual User User { get; set; }
+
+        [ForeignKey("CategoryID")]
+        public virtual Category Category { get; set; }
+
+        // list of Answer 
+        //public virtual List<Answers> Answers { get; set; }
+    }
+}
---
+++ StackOverflowProject.DomainModel/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StackOverflowProject.DomainModel
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // For generate identity id
+        public int UserID { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Name { get; set; }
+        public string Mobile { get; set; }
+        public string IsAdmin { get; set; }
+    }
+
+}
